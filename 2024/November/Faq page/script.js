974a88f2553894552ce8d497b4c9cda8118ea0b8
OferS94 <ofersela94@gmail.com>
2024-11-13 12:24:48 +0200

    ugly project

--- 2024/November/Faq page/script.js
+++ 2024/November/Faq page/script.js
@@ -1,20 +1,83 @@
 
-document.addEventListener("DOMContentLoaded", ()=> {
-    const togButt = document.querySelectorAll(".buttTog");
-    const toggledText = document.querySelectorAll(".togText")[index];
-    togButt.forEach((button, index) => {
-        button.addEventListener("click", () => {
-            
 
-            if(toggledText.style.display === "none" && toggledText.style.display === ""){
-               toggledText.style.display = "block";
-            button.style.backgroundImage = "url('assets/images/icon-minus.svg')";
-            } else {
-                toggledText.style.display = "none";
-                button.style.backgroundImage = "url('assets/images/icon-plus.svg')";
-            }
+document.addEventListener("DOMContentLoaded", ()=> {
+    const togButts = document.querySelectorAll(".buttTog");
+    const toggledText = document.querySelectorAll(".bigText");
+    
+    togButts.forEach((button, index) => {
+        button.addEventListener("click", () => {
+            const images = button.querySelectorAll("img")
+            // console.log(images)
+                images.forEach((img ) => {
+                    img.classList.toggle('disabled')
+                })
+            // console.log(`button ${index} clicked`)
+
+            toggledText[index].classList.toggle('disabled');
+            // console.log(toggledText[index]);
         })
     })
 })
+
+
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", ()=> {
+//     const togButts = document.querySelectorAll(".buttTog");
+//     const toggledText = document.querySelectorAll(".bigText");
+    
+//     togButts.forEach((button, index) => {
+//         button.addEventListener("click", () => {
+//             const images = button.querySelectorAll("img")
+//             console.log(images)
+//                 images.forEach((img ) => {
+//                     img.classList.toggle('disabled')
+//                 })
+//             // console.log(`button ${index} clicked`)
+
+//             toggledText[index].classList.toggle('disabled');
+//             console.log(toggledText[index]);
+//         })
+//     })
+// })
+//////////////////////////////////////////////
+
+
+
+
+
+//////////////////////////////////////////////////////
+// const contents = document.querySelector('.contents');
+// console.log("con");
+
+// contents.addEventListener("click", (ev) =>{
+//     if (ev.target.classList.contains("buttTog")){
+//         console.log(`this is button`);
+        
+
+//     }
+// })
+
+
+
+            // if (toggledText[index].style.display === "none"){
+            //     toggledText[index].style.display = "block";
+            //     button.style.backgroundImage = "url('assets/images/icon-minus.svg')";
+            // } else {
+            //     toggledText[index].style.display = "none";
+            //     button.style.backgroundImage = "url('assets/images/icon-plus.svg')";
+            // }
+            // if (toggledText[index].style.display === "none"){
+            //     toggledText[index].classList.toggle('.disabled')
+            //     button.style.backgroundImage = "url('assets/images/icon-minus.svg')";
+            // } else {
+            //     toggledText[index].classList.toggle('.disabled')
+            //     button.style.backgroundImage = "url('assets/images/icon-plus.svg')";
+            // }
 
 
